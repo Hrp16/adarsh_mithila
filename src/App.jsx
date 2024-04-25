9b27ddbf0efad30a8b0ac1263c8ac0cea8b4a93b
Standard <standard@192.168.1.6>
2024-04-25 23:13:54 +0530

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+import HomePage from './Main/homePage';
+
+function App() {
+  return (
+    <body>
+      <HomePage />
+    </body>
+  );
+}
+export default App;
---
+++ src/Main/homePage.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import styled from "styled-components";
+import map from "../images/Map.jpeg";
+
+const homePage = ()  => {
+    return (
+      <Wrap>
+        <TitleWrap>
+          <Title>Adarsh mithila party</Title>
+        </TitleWrap>
+        <Map><img src={map} alt="map"/></Map>
+      </Wrap>
+    );
+  };
+const Wrap = styled.main`
+`;
+const TitleWrap =styled.section`
+    display: flex;
+    justify-content: center;
+    background: tan;
+  `;
+  const Title = styled.span`
+  font-size: 64px;
+  font-family: fantasy;
+  margin-top: 16px;
+  color: darkgreen;
+  `;
+
+  const Map = styled.span`
+  display: flex;
+  justify-content: center;
+  background: whitesmoke;
+  img {
+    width: 700px;
+    }
+  }`;
+
+export default homePage;
+
